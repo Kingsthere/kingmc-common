b20aec6d49181da3b43e10c50cddf12f906a494c
kingsthere <kingsthere0@hotmail.com>
2023-05-30 18:40:58 +0800

    add getPropertyOrElse and getPropertyOrNull

--- application/src/main/kotlin/kingmc/common/application/ApplicationProperties.kt
+++ application/src/main/kotlin/kingmc/common/application/ApplicationProperties.kt
@@ -7,3 +7,19 @@ package kingmc.common.application
  * @author kingsthere
  */
 fun getProperty(key: String): String = currentApplication().properties.getProperty(key)
+
+/**
+ * Gets a property or default value from [currentApplication]
+ *
+ * @since 0.0.9
+ * @author kingsthere
+ */
+fun getPropertyOrElse(key: String, defaultValue: String): String = currentApplication().properties.getProperty(key, defaultValue)
+
+/**
+ * Gets a property or `null` from [currentApplication]
+ *
+ * @since 0.0.9
+ * @author kingsthere
+ */
+fun getPropertyOrNull(key: String): String? = currentApplication().properties.getProperty(key)
