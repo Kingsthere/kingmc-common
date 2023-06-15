e30aa516fb555749f5d27fac3ae108399729c376
kingsthere <kingsthere0@hotmail.com>
2023-06-15 18:47:39 +0800

    Add toText

--- common/src/main/kotlin/kingmc/util/text/TextDisplayable.kt
+++ common/src/main/kotlin/kingmc/util/text/TextDisplayable.kt
@@ -16,3 +16,28 @@ interface TextDisplayable {
      */
     fun asText(): Text
 }
+
+val TEXT_NULL = Text("null")
+
+/**
+ * Converts this object into a [Text] reference, if this object implements [TextDisplayable]
+ * interface it will give the [TextDisplayable.asText] as result, otherwise it will return a
+ * plain text with literal [toString] as result
+ *
+ * @receiver object to convert as a text
+ * @return text converted
+ */
+fun Any?.toText(): Text {
+    return if (this == null) {
+        // Simply return null
+        TEXT_NULL
+    } else {
+        if (this is TextDisplayable) {
+            // Return TextDisplayable.asText() as result
+            this.asText()
+        } else {
+            // Return plain text with "this.toString()" as result
+            Text(this.toString())
+        }
+    }
+}
